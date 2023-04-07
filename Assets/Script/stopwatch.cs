90a5c18eae99f30b710898602e42646a1d635350
IkeaBlahaj <chenvincent306@gmail.com>
2023-04-07 16:46:01 +0800

    +計時器

---
+++ Assets/Script/stopwatch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class stopwatch : MonoBehaviour
+{
+    public float min, sec, milisec, currentTime = 0;
+    public int k = 0;
+    public Text txt;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(k % 2 != 0) {
+            currentTime += Time.deltaTime;
+            min = Mathf.Floor(currentTime / 60);
+            sec = currentTime % 60;
+            txt.text = string.Format("{0:00}:{1:00.00}",min,sec); }        
+    }
+    private void OnTriggerEnter(Collider other) {
+        k++;
+    }
+}
